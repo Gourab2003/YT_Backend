b3fc47d6bf108a0cb8a111a531fd56d0a7786c9c
Gourab <roygourab005@gmail.com>
2024-02-18 12:09:19 +0530

    update the index.js file in db folder

--- src/db/index.js
+++ src/db/index.js
@@ -3,11 +3,8 @@ import { DB_NAME } from "../constants.js";
 
 const connect_DB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}?retryWrites=true&w=majority&dbName=${DB_NAME}`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log(connectionInstance);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        //console.log(connectionInstance);
         console.log(`\nMongoDB connected! DB Host: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.error("Error in db connect:", error);
